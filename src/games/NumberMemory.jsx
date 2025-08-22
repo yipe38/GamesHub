81a59deb75fef619d38cea8f9fa210f0b7e99d80
yipe38 <lukasjohanneswolf@outlook.de>
2025-08-22 13:13:43 +0200

    Update NumberMemory.jsx

--- src/games/NumberMemory.jsx
+++ src/games/NumberMemory.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
+import Button from "../components/Button.jsx";
 
 function Button({ children, onClick, variant = "default", type = "button" }) {
   const base =
