bd424b3b84a7b0f141182e5a4dea5b836aec6ffc
yipe38 <lukasjohanneswolf@outlook.de>
2025-08-22 13:19:18 +0200

    Update SnakeGame.jsx

--- src/games/SnakeGame.jsx
+++ src/games/SnakeGame.jsx
@@ -1,5 +1,6 @@
 
 import React, { useEffect, useRef, useState } from "react";
+import Button from "../components/Button.jsx";
 
 function Button({ children, onClick, variant = "default" }) {
   const base =
